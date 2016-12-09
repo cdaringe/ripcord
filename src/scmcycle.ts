fe03f40ccdfbf746e7cf757adfdca2fb48fd0153
cdaringe <christopher.dieringer@gmail.com>
2016-12-09 14:06:10 -0800

    fix(cycle): fix prop extraction

--- src/scmcycle.ts
+++ src/scmcycle.ts
@@ -20,7 +20,7 @@ function _run (opts: any): Promise<any> {
   const pkgFilename = counsel.targetProjectPackageJsonFilename
   const pkgNameVersion = `${pkg.name}@${pkg.version}`
   const publishRegistryUri = npm.config.get('publish_registry')
-  const { branch, revision } = process.env.revision
+  const { branch, revision } = process.env
 
   const readyToCycle = validatePrereqs()
   /* istanbul ignore next */
