6817b1ab55f8e884336ee91d097fa46d70dc785f
cdaringe <christopher.dieringer@gmail.com>
2017-01-11 17:16:33 -0800

    fix(install): false completion logging

--- scripts/install.js
+++ scripts/install.js
@@ -2,15 +2,16 @@
 const cp = require('child_process');
 const path_1 = require('path');
 const counsel_1 = require('counsel');
-try {
-    const root = counsel_1.project.findProjectRoot(path_1.resolve(__dirname, '..', '..'));
-    cp.exec('node bin/ripcord.js counsel apply', { cwd: path_1.resolve(__dirname, '..') });
-    console.log('[ripcord] install success');
-}
-catch (err) {
+const exec = pify(cp.exec);
+Promise.resolve()
+    .then(() => counsel_1.project.findProjectRoot(path_1.resolve(__dirname, '..', '..')))
+    .catch(err => {
     console.warn([
         'unable to install ripcord rules. if this was a global install, fear not',
         `and proceed. ${err.message}`
     ].join(' '));
-}
+    process.exit(0);
+})
+    .then(() => exec('node bin/ripcord.js counsel apply', { cwd: path_1.resolve(__dirname, '..') }))
+    .then(() => console.log('[ripcord] install success'));
 //# sourceMappingURL=install.js.map
--- scripts/install.ts
+++ scripts/install.ts
@@ -1,14 +1,16 @@
 import * as cp from 'child_process'
 import { resolve } from 'path'
 import { project } from 'counsel'
+const exec = pify(cp.exec)
 
-try {
-  const root = project.findProjectRoot(resolve(__dirname, '..', '..'))
-  cp.exec('node bin/ripcord.js counsel apply', { cwd: resolve(__dirname, '..') })
-  console.log('[ripcord] install success')
-} catch (err) {
+Promise.resolve()
+.then(() => project.findProjectRoot(resolve(__dirname, '..', '..')))
+.catch(err => {
   console.warn([
     'unable to install ripcord rules. if this was a global install, fear not',
     `and proceed. ${err.message}`
   ].join(' '))
-}
+  process.exit(0)
+})
+.then(() => exec('node bin/ripcord.js counsel apply', { cwd: resolve(__dirname, '..') }))
+.then(() => console.log('[ripcord] install success'))
